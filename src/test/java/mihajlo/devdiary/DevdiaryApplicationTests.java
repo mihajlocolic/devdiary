8d671c80b325a3b8ba341618b0f89f6f2f5b0734
Mihajlo Colic <colicmihajlo@gmail.com>
2025-07-10 08:35:02 +0200

    Initial commit

---
+++ src/test/java/mihajlo/devdiary/DevdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package mihajlo.devdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
